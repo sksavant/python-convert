12c45970be668d51b1c28d0908416bc25156cb04
Savant Krishna <savant.2020@gmail.com>
2016-09-08 06:58:49 +0530

    tag as beta

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
 
 distutils.core.setup(
 	name='pyconvert',
-	version = '0.6.3',
+	version = '0.6.3beta',
 	packages=['pyconvert','demos'],
 	author='Innovaser',
 	author_email='rancavil@innovaser.cl',
